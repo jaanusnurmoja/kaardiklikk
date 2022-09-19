8c1a3142c80fa17af737c83658dea704be9e979a
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-09-19 22:56:47 +0300

    ei midagi erilist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KaartComponent } from './kaart/kaart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    KaartComponent
-  ],
+  declarations: [AppComponent, KaartComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
