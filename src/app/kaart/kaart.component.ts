47b52c8e54fa36a0a25632b23edb3b3fed68186f
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2022-09-20 14:21:18 +0300

    näidisrakendus valmis

--- src/app/kaart/kaart.component.ts
+++ src/app/kaart/kaart.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+//import { HTTPParser, HTTPParserConstructor } from 'http-parser-js';
+import { HttpClient } from '@angular/common/http';
+import * as pathExists from 'path-exists';
 
 @Component({
   selector: 'app-kaart',
@@ -10,8 +13,13 @@ export class KaartComponent implements OnInit {
   currentMap: any = { title: 'string', path: 'string' };
   sidebar: boolean = false;
   nomap: any;
+  noimg: any;
+
   //clickArea: any;
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private httpClient: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.getRouteParams();
@@ -21,9 +29,25 @@ export class KaartComponent implements OnInit {
     const kaart = this.activatedRoute.snapshot.paramMap.get('kaart');
 
     if (kaart && this.mapListContainsPath(kaart)) {
-      this.currentMap.title = kaart.toUpperCase();
-      this.currentMap.path = kaart;
       this.setSidebar(true);
+      let imgExists = this.fileExist('/assets/img/map/' + kaart + '.png');
+      if (imgExists === true) {
+        this.getMapList().forEach((thisMap) => {
+          if (thisMap.path === kaart) {
+            this.currentMap.title = thisMap.title;
+          }
+        });
+        this.currentMap.path = kaart;
+      } else {
+        this.currentMap.path = 'hybriid';
+        this.getMapList().forEach((thisMap) => {
+          this.currentMap.title = thisMap.title;
+          if (thisMap.path === kaart) {
+            this.noimg =
+              'Kaardipilt peaks olemas olema, kuid seda ei õnnestunud laadida. Palume kindlasti probleemist teavitada tugi@misiganes.ee';
+          }
+        });
+      }
     } else {
       if (kaart) {
         this.nomap =
@@ -45,13 +69,21 @@ export class KaartComponent implements OnInit {
     return containsPath;
   }
 
+  fileExist(urlToFile: string) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('HEAD', urlToFile, false);
+    xhr.send();
+
+    return xhr.status !== 404;
+  }
+
   getMapList(): any[] {
     return [
       { title: 'Hübriid', path: 'hybriid' },
       { title: 'Ortofoto', path: 'ortofoto' },
       { title: 'Reljeef', path: 'reljeef' },
       { title: 'Põhikaart', path: 'pohikaart' },
-      //{ title: 'Olematu', path: 'olematu' },
+      { title: 'Failitu', path: 'failitu' },
     ];
   }
   setSidebar(show: boolean): void {
